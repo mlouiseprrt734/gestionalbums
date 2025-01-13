f414df92e5042107eecf25d12eddb61e6f6373db
Marie-Louise Porret <marielouiseporret@gmail.com>
2025-01-13 10:02:21 +0100

    ajout theme sombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:gestionalbums/HomeButton.dart';
-import 'package:gestionalbums/MusicButton.dart';
-import 'package:gestionalbums/SettingButton.dart';
+import 'package:gestionalbums/AppBar/custom_icons_icons.dart';
+import 'package:json_theme/json_theme.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'AppBar/appbar.dart';
 
-void main() {
-  runApp(const MyApp());
+//controller pour le thème
+class ThemeController extends ValueNotifier<ThemeMode> {
+  ThemeController() : super(ThemeMode.light);
+
+  void toggleTheme() {
+    value = value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
+}
+
+final themeController = ThemeController();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final themeStr =
+      await rootBundle.loadString('assets/appainter_theme_light.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  final themeStr_dark =
+      await rootBundle.loadString('assets/appainter_theme_dark.json');
+  final themeJson_dark = jsonDecode(themeStr_dark);
+  final theme_dark = ThemeDecoder.decodeThemeData(themeJson_dark)!;
+
+  runApp(MyApp(theme: theme));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeData theme;
+  const MyApp({Key? key, required this.theme}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gestion d\'albums',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Gestion d\'albums Home Page'),
-    );
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: themeController,
+        builder: (context, themeMode, _) {
+          return MaterialApp(
+            title: 'Gestion d\'albums',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const MyHomePage(title: 'Gestion d\'albums Home Page'),
+          );
+        });
   }
 }
 
@@ -53,6 +82,12 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: const Icon(Icons.search),
               onPressed: () {
                 // do something
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                ThemeController().toggleTheme();
               },
             ),
           ]),
@@ -88,12 +123,12 @@ class _MyHomePageState extends State<MyHomePage> {
             label: "Home",
           ),
           NavigationDestination(
-            icon: const Icon(MusicButton.home),
+            icon: const Icon(CustomIcons.queue_music),
             label: "Liste Albums",
           ),
           NavigationDestination(
             selectedIcon: Icon(Icons.bookmark),
-            icon: const Icon(SettingButton.home),
+            icon: const Icon(CustomIcons.cog_alt),
             label: "Paramètres",
           ),
         ],
